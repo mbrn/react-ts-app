ad6e1927090949ece55302fb4fa30b63d5398b5f
Mehmet Baran <mehmetbaran@mehmetbaran.net>
2018-10-07 14:18:16 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+export interface AppProps {
+}
+
+export default class App extends React.Component<AppProps, undefined> {
+    render() {
+        return (
+            <div className="app">
+                <h1>Hello World!</h1>
+                <p>Foo to the barz</p>
+            </div>
+        );
+    }
+}
